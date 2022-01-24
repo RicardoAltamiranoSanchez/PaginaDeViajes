748611f12f13e0001dd9de6b540605e377e2bce0
Ricardo <altamiranoricardo545@gmail.com>
2022-01-23 22:34:18 -0600

    Lista la pagina modificamos la base de dato ya que nos daba errore al momento de subirlo a produccion y modificamos el footer para que se vea un poco mejor

--- config/db.js
+++ config/db.js
@@ -1,13 +1,13 @@
 //configuracion de la base de datos
 import Sequelize from 'sequelize';
 //Pasamos la variables para produccion
-import dotenv from'dotenv';//asi en la forma de import en require no funciona creo que por que esta activado el import en el pack json 
-dotenv.config({path:'variables.env'});
+import dotenv from'dotenv';
+dotenv.config({path:'variable.env'});
 // primero ponemos el nombre de la baje de datos y depues el usuario y el password
 
 const db=new Sequelize(process.env.DB_BASEDATOS,process.env.DB_USUARIO,process.env.DB_PASSWORD,{
  host:process.env.DB_HOST,
-port:process.env.DB_PUERTO,
+ port:process.env.DB_PUERTO,
 dialect:'mysql',
 define:{
 
--- index.js
+++ index.js
@@ -7,23 +7,23 @@
 //   "license": "ISC",
 //   "dependencies": {
 //     "express": "^4.17.2"
-//   }
+//}
+
 import express from 'express';
 import router from './Router/router.js';
 import db from './config/db.js'
+
 const app=express();
 
-
-
-
-
-
-
+//hacemos el entorno para producion y Desarrollador
+const HOST = process.env.HOST || '0.0.0.0';
+const PORT = process.env.PORT || 5000;
 
 //conexion ala bae de datos
 db.authenticate()
 .then(()=>{console.log("Conexion ala base de datos existosa");})
 .catch((error)=>{console.log(`hubo un error en ${error}`)});
+
 
 //para habilitar un template engine instalamos npm install pug
 //habilitamos pug
@@ -38,26 +38,21 @@ const titulo="Agencia De Viajes";
 res.locals.ActualYear=year.getFullYear();
 res.locals.tituloIndex=titulo;
 next();
-
 });
 
 //definimos la carpeta publica
 app.use(express.static('public'));
 //agregamos los router
 app.use('/',router);
-//hacemos el entorno para producion y Desarrollador
-const host = process.env.HOST || '0.0.0.0';
-const port =process.env.PORT || 5000;
 
 
-app.listen(port,host,()=>{
-console.log(`Servidor Iniciado Correctamente`);
+app.listen(PORT,()=>{
+console.log(`Servidor Iniciado Correctamente en el puerto${PORT}`);
 });
 
 // instalamos dependiencias para la conexion de la base de datos en mysql
 //npm install mysql2
 //npm install sequelize este seria un orm
-
 //pagina para cambiar codigo de html a pug https://html-to-pug.com/
 //pagina de iconos svg https://tablericons.com/
 // npm install --save-dev dotenv instalar variable de entorno
